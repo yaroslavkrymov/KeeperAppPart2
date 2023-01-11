d9d6a10fe62117fdaa90627b6b0cbf7eb49968f9
yaroslavkrymov <93751242+yaroslavkrymov@users.noreply.github.com>
2023-01-11 09:24:54 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {notes.map((createNotes) => (
+        <Note
+          key={createNotes.key}
+          title={createNotes.title}
+          content={createNotes.content}
+        />
+      ))}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
